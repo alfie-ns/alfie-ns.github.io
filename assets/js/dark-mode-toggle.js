2cecbbf58d5e96f6a06145df3ec40aea98e7112d
alfie-ns <alfienurse@gmail.com>
2024-05-21 15:21:42 +0100

    update

--- assets/js/dark-mode-toggle.js
+++ assets/js/dark-mode-toggle.js
@@ -1,23 +1,25 @@
-(function() {
-    var toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-    var currentTheme = localStorage.getItem('theme');
-  
-    if (currentTheme) {
-      document.documentElement.setAttribute('data-theme', currentTheme);
-      if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-      }
+document.addEventListener("DOMContentLoaded", function() {
+  var toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+  var currentTheme = localStorage.getItem('theme');
+
+  if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+      toggleSwitch.checked = true;
     }
-  
-    function switchTheme(e) {
-      if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-      } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-      }    
+  }
+
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
     }
-  
+  }
+
+  if (toggleSwitch) {
     toggleSwitch.addEventListener('change', switchTheme, false);
-  })();
+  }
+});
