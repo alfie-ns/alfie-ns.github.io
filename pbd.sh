2b4f03105bbb2d6d6e94154967ff7ca3e3bac02c
alfie-ns <alfienurse@gmail.com>
2024-06-10 14:16:01 +0100

    update

--- pbd.sh
+++ pbd.sh
@@ -6,42 +6,30 @@
 
 # Function to handle errors
 handle_error() {
-    local exit_code="$1" # '$1' is the first argument passed to the function, assigned to 'exit_code' as the exit status code.
-    local msg="$2"       # '$2' is the second argument passed to the function, assigned to 'msg' as the error message.
+    local exit_code="$1"  # '$1' is the first argument passed to the function, assigned to 'exit_code' as the exit status code.
+    local msg="$2"        # '$2' is the second argument passed to the function, assigned to 'msg' as the error message.
     echo "Error: $msg" >&2 # Output the error message to standard error (stderr).
-    exit "$exit_code"    # Exit the script with the provided exit code.
+    exit "$exit_code"     # Exit the script with the provided exit code.
 }
 
-# Critical command example
-# some_critical_command || handle_error 4 "Critical command failed."
-
 # Verify push.sh's existence and executability
-# if push.sh is not found, exit with error(2)
 [[ ! -f "./push.sh" ]] && handle_error 2 "push.sh is missing."
-
-# if push.sh is not executable, exit with error(3)
 [[ ! -x "./push.sh" ]] && handle_error 3 "push.sh is not executable."
 
 # Execute push.sh
-# if it fails, script stops due to 'set -e'
-./push.sh
+if ./push.sh; then
+    # If push.sh succeeds
+    echo "Pushed to GitHub; backing out to parent directory..."
+    cd ..
 
-# Post-push actions: navigating up and cleaning the directory
-echo "Pushed to GitHub; backing out to parent directory..."
-cd ..
+    # Remove alfie-ns.github.io directory recursively
+    echo "Deleting local repository..."
+    rm -rf alfie-ns.github.io
 
-sleep 2 # sleep for 2 seconds
-
-# Remove alfie-ns.github.io directory recursively
-# if rm fails, script stops due to 'set -e'
-echo "deleting local repository..."
-rm -rf alfie-ns.github.io
-
-
-# 'alfie-ns' ASCII Art
-cat <<'EOF'
+    # 'alfie-ns' ASCII Art
+    cat <<'EOF'
 -----------------------------------------
- ⚙️ Process complete ⚙️                   |
+| ⚙️ Process complete ⚙️                  |
 -----------------------------------------
 |         _  __ _                       |
 |   __ _ | |/ _(_) ___       _ __  ___  | 
@@ -49,5 +37,9 @@ cat <<'EOF'
 | | (_| || |  _| |  __/_____| | | \__ \ |
 |  \__,_||_|_| |_|\___|     |_| |_|___/ |
  ---------------------------------------- 
- 
 EOF
+
+else
+    # If push.sh fails
+    handle_error $? "Execution of push.sh failed."
+fi
