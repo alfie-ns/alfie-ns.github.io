a41162e213c5c85f9d8ce27499310a4a3f50599b
alfie-ns <alfienurse@gmail.com>
2024-07-01 04:12:29 +0100

    update

--- pu.sh
+++ pu.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-
 # vidbriefs-app/push.sh
+
+# Function to print bold text
+print_bold() {
+  BOLD=$(tput bold)
+  NORMAL=$(tput sgr0)
+  echo -e "\n${BOLD}$1${NORMAL}\n"
+}
 
 git add .
 git commit -m "update"
 git push origin main
+
+print_bold "PUSHED TO GITHUB"
