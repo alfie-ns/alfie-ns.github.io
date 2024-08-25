f42d8bcb1c2407e7a8932dd7a976d2ecb0d49bb2
alfie-ns <alfienurse@gmail.com>
2024-08-25 13:33:20 +0100

    update

--- pu.sh
+++ pu.sh
@@ -8,8 +8,9 @@ print_bold() {
   echo -e "\n${BOLD}$1${NORMAL}\n"
 }
 
-git add .
-git commit -m "update"
-git push origin main
-
-print_bold "PUSHED TO GIT"
+if git add . && git commit -m 'update'; then
+  git push origin main
+  print_bold "PUSHED TO GIT"
+else
+  print_bold "FAILED TO PUSH TO GIT"
+fi
